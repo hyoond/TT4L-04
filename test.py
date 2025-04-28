02bf5d3a8defd34e65b5c1391a4fc1dff5081ad0
hyoond <hoeyoond@gmail.com>
2025-04-28 21:19:39 +0800

    Testing out sqlite and created a database to test simple user signup and user login function

--- test.py
+++ test.py
@@ -1,8 +1,65 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
 test = Flask(__name__)
 
 @test.route('/')
-def testing():
-  return 'Hello World'
+def signup_form():
+    return render_template('usersignup.html')
+
+@test.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+
+    insert_user(username, password)
+    return f"User {username} added successfully! Please <a href='/login'>login here</a>."
+
+@test.route('/login')
+def login_form():
+    return render_template('userlogin.html')
+
+@test.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = compare_database(username, password)
+
+    if user:
+        return f"Welcome, {username}!"
+    else:
+        return "User not found or Incorrect password."
+
+def compare_database(username, password):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    SELECT * FROM users WHERE name = ? AND password = ?
+    ''', (username, password))
+
+    user = cursor.fetchone()
+    conn.close()
+    return user
+
+def insert_user(username, password):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+       CREATE TABLE IF NOT EXISTS users (
+           id INTEGER PRIMARY KEY AUTOINCREMENT,
+           name TEXT NOT NULL,
+           password TEXT NOT NULL
+       )
+    ''')
+
+    cursor.execute('''
+    INSERT INTO users (name, password) VALUES (?, ?)
+    ''', (username, password))
+
+    conn.commit()
+    conn.close()
 
 test.run(debug=True)
